6c78ef56ac545a9c247383b803f87fa5d0f3cea2
khays9 <khays@illinois.edu>
2018-04-13 12:58:08 -0500

    Corrected Midwest homepage example 4
    Fixed multiple accessibility errors in the homepage. Tabs now use the
    tab panel widget from the ARIA examples.

--- examples/midwest/js/aria.js
+++ examples/midwest/js/aria.js
@@ -2,53 +2,6 @@ $(document).ready(function() {
       var topmenu = new menu('topmenu', false);
       var mainMenu = new menu('main-menu', true);
 
-      /////////////////// home page event handlers and functions /////////////////////////
-
-      if ($('.quicktab').length > 0) {
-         $('.quicktab').focus(function(e) {
-
-            // set active-tab styling on the selected tab, removing it from the others
-            $(this).siblings().removeClass('active-tab');
-            $(this).addClass('active-tab');
-         });
-
-         $('.quicktab').click(function(e) {
-
-            doPanelSwap($(this));
-
-            e.stopPropagation();
-            return false;
-         });
-
-         $('.quicktab').keydown(function(e) {
-
-            var enterKey = 13;
-
-            if (e.altKey||e.ctrlKey||e.shiftKey) {
-               // do nothing
-               return true;
-            }
-
-            if (e.keyCode == enterKey) {
-               doPanelSwap($(this));
-               e.stopPropagation();
-               return false;
-            }
-
-            return true;
-         });
-
-         function doPanelSwap($id) {
-            var $panel = $('#' + $id.attr('aria-controls'));
-
-            // display the selected tab, hide the others, and update the aria attributes of the tabs
-            $panel.siblings('div').attr('aria-hidden', 'true').attr('aria-expanded', 'false');
-            $panel.attr('aria-hidden', 'false').attr('aria-expanded', 'true');
-
-            // set focus on the selected tab
-            $panel.focus();
-         }
-      }
 
       ////////////////// joblink functions ////////////////////////
       //
