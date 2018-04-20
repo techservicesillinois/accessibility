828176c0d0dae86092d302ea8b19f891147f51e3
khays9 <khays@illinois.edu>
2018-04-20 17:37:42 -0500

    IADP progress bar example - moved code to js file
    Moved inline script for progress element example to the js file.

--- js/progressbar.js
+++ js/progressbar.js
@@ -2,6 +2,45 @@ var intervalID = null;
 var progBar = null;
 var $progBtn;
 var bProgStarted = false;
+var bProgElStarted = false;
+var hProgElInterval = null;
+var progElBtn = document.getElementById("prog1-start");
+var progElem = document.getElementById("prog1");
+
+function doElProg() {
+   
+   if (!bProgElStarted) { // example not running
+      if (parseInt(progElem.getAttribute("value")) !== parseInt(progElem.getAttribute("max"))) {
+         hProgElInterval = setInterval(function() {
+
+            var val = parseInt(progElem.getAttribute('value'));
+            var max = parseInt(progElem.getAttribute('max'));
+         
+            if (val < max) {
+               progElem.setAttribute("value", val+10);
+            }
+            else {
+               clearInterval(hProgElInterval);
+               progElBtn.innerHTML = "Reset Progress Element"
+               bProgElStarted = false;
+            }
+         }, 500);
+         
+         progElBtn.innerHTML = "Stop Progress Element";
+         bProgElStarted = true;
+      }
+      else {
+         progElem.setAttribute("value", 0);
+         progElBtn.innerHTML = "Start Progress Element";
+         bProgElStarted = false;
+      }
+   }
+   else { // example is running
+      clearInterval(hProgElInterval);
+      progElBtn.innerHTML = "Start Progress Element";
+      bProgElStarted = false;
+   }
+}
 
 function doProgress() {
    if (!progBar.isMax()) {
